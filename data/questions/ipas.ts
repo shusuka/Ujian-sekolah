19091bd13b7d7ec2169c1756c652d86a4a99b333
shusuka <love201108@gmail.com>
2026-05-10 20:48:28 +0700

    fix: add ipasBank alias export for index.ts compatibility

--- data/questions/ipas.ts
+++ data/questions/ipas.ts
@@ -646,3 +646,6 @@ export const ipasQuestions: SubjectQuestionBank = {
     { pg: [24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0], is: [4,3,2,1,0,9,8,7,6,5], ut: [4,0,1,2,3] },
   ],
 };
+
+// Alias for backward compatibility with index.ts
+export const ipasBank = ipasQuestions;
